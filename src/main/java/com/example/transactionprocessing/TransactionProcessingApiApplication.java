76d842df571aadbb6581890fefa04457e9f96e55
Guilherme Abdelnor <guigo211103@gmail.com>
2026-02-10 12:52:08 -0300

    feat: initial Spring Boot project setup with healthcheck endpoint

---
+++ src/main/java/com/example/transactionprocessing/TransactionProcessingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionProcessingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionProcessingApiApplication.class, args);
+	}
+
+}
